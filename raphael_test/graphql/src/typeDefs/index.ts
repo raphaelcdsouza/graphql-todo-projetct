fb8a6a9e7473a999626d87068243c4083d5cdea8
Raphael Cassio de Souza <rapha.cassio@gmail.com>
2022-05-02 12:18:31 -0700

    [BE] chore(src/typeDefs.ts): ToDo type

--- raphael_test/graphql/src/typeDefs/index.ts
+++ raphael_test/graphql/src/typeDefs/index.ts
@@ -1,12 +1,20 @@
 import { gql } from 'apollo-server'
 
 export const typeDefs = gql`
+  type ToDo {
+    _id: String
+    name: String
+    description: String
+    category: String
+    user: String
+  }
+
   type Query {
     listToDos: String
   }
 
   type Mutation {
     login(email: String!): String!
-    toDo: String!
+    createToDo: ToDo!
   }
 `
